08a2cb0541eb59e0d1bfe7e2b5a7cfcf23a0ebed
Semyon Antoshkin <s.s.antoshkin@gmail.com>
2023-02-13 21:08:15 +0500

    Create templates

--- main.go
+++ main.go
@@ -1,1 +1,98 @@
 package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+type Rsvp struct {
+	Name, Email, Phone string
+	WillAttend         bool
+}
+
+var responses = make([]*Rsvp, 0, 10)
+var templates = make(map[string]*template.Template, 3)
+
+func loadTemplates() {
+	templateNames := [5]string{"welcome", "form", "thanks", "sorry", "list"}
+	for index, name := range templateNames {
+		t, err := template.ParseFiles("./templates/base.html", "./templates/"+name+".html")
+		if err == nil {
+			templates[name] = t
+			fmt.Println("Loaded template", index, name)
+		} else {
+			panic(err)
+		}
+	}
+}
+
+func welcomeHandler(writer http.ResponseWriter, request *http.Request) {
+	templates["welcome"].Execute(writer, nil)
+}
+
+type formData struct {
+	*Rsvp
+	Errors []string
+}
+
+func formHandler(writer http.ResponseWriter, request *http.Request) {
+	if request.Method == http.MethodGet {
+		templates["form"].Execute(writer, formData{
+			Rsvp:   &Rsvp{},
+			Errors: []string{},
+		})
+	} else if request.Method == http.MethodPost {
+		request.ParseForm()
+		responseData := Rsvp{
+			Name:       request.Form["name"][0],
+			Email:      request.Form["email"][0],
+			Phone:      request.Form["phone"][0],
+			WillAttend: request.Form["willattend"][0] == "true",
+		}
+
+		errors := []string{}
+		if responseData.Name == "" {
+			errors = append(errors, "Пожалуйста, введите Ваше имя")
+		}
+
+		if responseData.Email == "" {
+			errors = append(errors, "Пожалуйста, введите Ваш адрес электронной почты")
+		}
+
+		if responseData.Phone == "" {
+			errors = append(errors, "Пожалуйста, введите Ваш номер телефона")
+		}
+
+		if len(errors) > 0 {
+			templates["form"].Execute(writer, formData{
+				Rsvp: &responseData, Errors: errors,
+			})
+		} else {
+			responses = append(responses, &responseData)
+
+			if responseData.WillAttend {
+				templates["thanks"].Execute(writer, responseData.Name)
+			} else {
+				templates["sorry"].Execute(writer, responseData.Name)
+			}
+		}
+	}
+}
+
+func listHandler(writer http.ResponseWriter, request *http.Request) {
+	templates["list"].Execute(writer, responses)
+}
+
+func main() {
+	loadTemplates()
+
+	http.HandleFunc("/", welcomeHandler)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/list", listHandler)
+
+	err := http.ListenAndServe(":5000", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
